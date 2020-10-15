13067c5a7d8437b2d7b9d1b0b35103a1f77f7d7c
이준희 <junh0328@naver.com>
2020-10-15 20:28:12 +0900

    10.2.2 todos 배열에 새 객체 추가하기/ 완료

--- fe/src/chapter/Chapter10.js
+++ fe/src/chapter/Chapter10.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import todo0 from './imgs/chapter10/todo0.png';
 import todo1 from './imgs/chapter10/todo1.png';
 import todo2 from './imgs/chapter10/todo2.png';
 import todo3 from './imgs/chapter10/todo3.png';
@@ -9,6 +10,8 @@ import todo7 from './imgs/chapter10/todo7.png';
 import todo8 from './imgs/chapter10/todo8.png';
 import todo9 from './imgs/chapter10/todo9.png';
 import todo10 from './imgs/chapter10/todo10.png';
+import todo11 from './imgs/chapter10/todo11.png';
+import todo12 from './imgs/chapter10/todo12.png';
 
 const Chapter10 = () => {
 	return (
@@ -106,6 +109,80 @@ const Chapter10 = () => {
 				체크박스를 보여줄 것입니다. 아이콘과 더불어 todos의 객체로 전달 받은
 				'text'를 같이 보여줍니다. 뒤의 클래스인 remove 클래스는 후에 구성할
 				것입니다.
+			</p>
+			<img src={todo0} alt="todo0" className="chapter__imgs" />
+			<p>
+				지금까지 작성한 코드를 통해 보여지는 뷰(view)단을 모두 구성했습니다!{' '}
+				<br />
+				되짚어 보자면, App.js에서 우리가 useState를 통해 만든 배열 [ ... ] 안에
+				담긴 객체에 들어가는 키, 값을 props로 하여금 TodoList에서 매핑할 수 있게
+				하였고, 그 매핑을 통해 풀어진 객체들이 TodoListItem에서 보여지게 됩니다.
+				아직 onChange 기능과 onSubmit 기능을 만들지 않았기 때문에, TodoList에
+				추가를 하지는 못하지만, 아이콘을 클릭하면 생기는 반응 들은 확인할 수
+				있습니다.
+			</p>
+			<h3 className="chapter__sub">10.2.2 항목 추가 기능 구현하기</h3>
+			<p>
+				지금 까지의 내용은 useState를 통해 객체로 넘겨받은 todos를 TodoList에
+				props로 전달 받은 뒤, 이를 TodoListItem에서 보여주는 기능을
+				구현하였습니다. 앞으로는 TodoInsert컴포넌트에서 인풋 상태를 관리하고 App
+				컴포넌트에는 todos 배열에 새로운 객체를 추가하는 함수를 만들어 주어야
+				합니다.{' '}
+				<b>먼저 들어가기 전에 어떤 함수를 써서 구현해야 할 지 생각해 보세요</b>
+			</p>
+			<h4 className="chapter__sub">10.2.2.1 TodoInsert value 상태 관리하기</h4>
+			<p>
+				TodoInsert 컴포넌트에서 인풋에 입력하는 값은 관리할 수 있도록 useState를
+				사용하여 value라는 상태를 정의하겠습니다. 추가로 인풋에 넣어 줄 onChange
+				함수도 작성해 주어야 하는데요. 이 과정에서 컴포넌트가 리렌더링될 때 마다
+				함수를 새로 만드는 것이 아니라, 한 번 만들고 재사용할 수 있도록
+				useCallback Hook을 사용하겠습니다.
+			</p>
+			<img src={todo11} alt="todo11" className="chapter__imgs" />
+			<p>
+				사실 인풋은 value 값과 onChange가 설정되지 않더라도 입력을 할 수는
+				있습니다. 다만 리액트 컴포넌트 쪽에서 해당하는 인풋에 무엇이
+				입력되었는지 추적하지 못할 뿐이죠. 이런 경우 현재 state가 잘
+				업데이트되고 있는지 확인하려면, onChange 함수 안에서 콘솔 로그를
+				찍어보는 방법이 있습니다.
+			</p>
+			<h4 className="chapter__sub">10.2.2.2 todos 배열에 새 객체 추가하기</h4>
+			<p>
+				이번에는{' '}
+				<b>App 컴포넌트에서 todos 배열에 새 객체를 추가하는 onInsert 함수</b>를
+				만들어 보겠습니다. 이 함수에서는 새로운 객체를 만들 때마다 id 값에 1씩
+				더해 주어야 하는데요. id 값은 useRef를 사용하여 관리하겠습니다.{' '}
+			</p>
+			<p>
+				여기서 useState가 아닌 useRef를 사용하여 컴포넌트에서 사용할 변수를
+				만드는 이유는 무엇일까요?
+			</p>
+			<p>
+				id값은 렌더링되는 정보가 아니기 때문입니다. 예를 들어 이 값은 화면에
+				보이지도 않고, 이 값이 바뀐다고 해서 컴포넌트가 리렌더링될 필요도
+				없습니다. 단순히 새로운 항목을 만들 때 참조되는 값일 뿐입니다.
+			</p>
+			<p>
+				또한 onInsert 함수는 컴포넌트의 성능을 아낄 수 있도록 useCallback으로
+				감싸 주겠습니다. 후에 onInsert 함수를 만든 뒤에 해당 함수를 TodoInsert
+				컴포넌트의 props로 설정합니다.
+			</p>
+			<p>
+				<img src={todo12} alt="todo12" className="chapter__imgs" />
+				App.js에서 새로 추가되는 부분만을 코드에서 캡쳐해 왔습니다. todos 배열에
+				새로운 객체를 추가해야 하므로, 기존 객체 3개 다음 값인 useRef를 (4)로
+				주고 nextId 변수에 담았습니다.
+			</p>
+			<p>
+				onInsert에서 첫번째 변수로 담기는 text는 todos 객체의 키인 text로,
+				TodoListItem에서 children으로 받아져 화면 상에 보여지게 될 것입니다.
+				또한 기존에 만든 todos 객체에서 4번째로 추가되기 때문에 설정했던
+				nextId의 현재값(current)를 id로 주었고, 기본적으로 새로 추가한 객체이기
+				때문에 checked는 false를 주었습니다. 또한 useState문의 todos 값을
+				설정하는 SetTodos를 만나, todos 객체에 우리가 insert 시킨 todo를
+				concat(이어 붙일 것)합니다. 후에 nextId는 1을 추가시킵니다. 밑에 배열에
+				담긴 [todos]는 우리가 배운 Hooks들 중 useEffect와 관계가 있습니다.
+				오로지 [todos] 객체에만 반응하는 onInsert를 만들기 위함입니다.
 			</p>
 		</div>
 	);
