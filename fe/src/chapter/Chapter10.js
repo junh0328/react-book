7ea25744b97956f242037ed1b84966708a17f07f
이준희 <junh0328@naver.com>
2020-10-15 19:54:11 +0900

    10.2.1 useState를 통한 todos의 상태 구현

--- fe/src/chapter/Chapter10.js
+++ fe/src/chapter/Chapter10.js
@@ -5,6 +5,10 @@ import todo3 from './imgs/chapter10/todo3.png';
 import todo4 from './imgs/chapter10/todo4.png';
 import todo5 from './imgs/chapter10/todo5.png';
 import todo6 from './imgs/chapter10/todo6.png';
+import todo7 from './imgs/chapter10/todo7.png';
+import todo8 from './imgs/chapter10/todo8.png';
+import todo9 from './imgs/chapter10/todo9.png';
+import todo10 from './imgs/chapter10/todo10.png';
 
 const Chapter10 = () => {
 	return (
@@ -40,10 +44,11 @@ const Chapter10 = () => {
 				content 클래스에 속한 내용은 JSX의 props로 children을 받게 됩니다.
 				컴포넌트 사이에 둘러 쌓인 값을 사용한다고 보면 됩니다.
 				<img src={todo3} alt="todo3" className="chapter__imgs" />
+				위의 이미지는 추후 완성된 App.js의 컴포넌트 내용입니다. TodoTemplate이
+				감싸고 있는 컴포넌트 TodoInsert, TodoList 가 바로 children으로 받게 될
+				내용입니다.
 			</p>
-			위의 이미지는 추후 완성된 App.js의 컴포넌트 내용입니다. TodoTemplate이
-			감싸고 있는 컴포넌트 TodoInsert, TodoList 가 바로 children으로 받게 될
-			내용입니다.
+
 			<h3 className="chapter__sub">10.1.2 TodoInsert 만들기</h3>
 			<img src={todo4} alt="todo4" className="chapter__imgs" />
 			<p>
@@ -57,6 +62,51 @@ const Chapter10 = () => {
 			<h3 className="chpater__sub">10.1.3 TodoListItem과 TodoList 만들기</h3>
 			<img src={todo5} alt="todo5" className="chapter__imgs" />
 			<img src={todo6} alt="todo6" className="chapter__imgs" />
+			<p>
+				TodoListItem들은 TodoList에 들어갈 컴포넌트로써 현재는 우리가 텍스틀
+				사전에 주어 작성하였지만, 후에는 props를 통해 클라이언트가 입력한 값을
+				넘겨 받아 매핑하여 뿌려줄 것입니다. 그 컴포넌트들(TodoListItem)을
+				TodoList에 담을 것입니다.
+			</p>
+			<h2 className="chapter__sub">10.2 기능 구현하기</h2>
+			<h3 className="chapter__sub">10.2.1 App에서 todos 상태 사용하기</h3>
+			<p>
+				우리가 view단을 구성할 때, TodoListItem에서 "할 일" 을 text 클래스로
+				묶어서 줬었습니다. 따라서 화면에 보여지기로는 할 일 / 할 일 / 할 일 로
+				표현되었을 것입니다. 하지만, 나중에 추가할 일정 항목에 대한
+				상태(state)는 모두 App 컴포넌트에서 관리합니다. App에서 useState를
+				사용하여 todos라는 상태를 정의하고, todos를 TodoList의 props로 전달할
+				것입니다.
+				<img src={todo7} alt="todo7" className="chapter__imgs" />
+				<img src={todo8} alt="todo8" className="chapter__imgs" />훅 함수인
+				useState를 배열로 사용하여, 그 안에 '할 일'에 해당되는 객체를
+				생성하였습니다. 각 객체는 json 형식을 이용하여 객체를 생성하였습니다.
+				또한 객체를 구분하기 위해 id를 주었고{' '}
+				<b>
+					(구분하지 않으면 콘솔창에서 고유한 id가 필요하다는 오류 메세지를
+					띄우고, 매핑 시에 index를 줘야하기 때문에 id를 주는 것이 좋습니다.)
+				</b>
+				, text는 실제로 표현될 할 일에 해당되고, checked는 토글 버튼을 통해
+				체크가되면 지워지는 모습을 보여주기 위해 만들 키입니다. TodoList의
+				props로 todos를 줌으로써, todoList props로 해당 키 값을 받아와
+				TodoListItem에 뿌려줄 수 있습니다.
+			</p>
+			<p>
+				<img src={todo9} alt="todo9" className="chapter__imgs" />
+				props로 받아 온 todos 배열을 배열 내장 함수 map을 통해 TodoListItem으로
+				이루어진 배열로 변환하여 렌더링해 주었습니다.
+			</p>
+			<p>
+				<img src={todo10} alt="todo10" className="chapter__imgs" />
+				이제 TodoList 컴포넌트에서 받아 온 todo값에 따라 제대로 된 UI를 보여 줄
+				수 있도록 컴포넌트를 수정하겠습니다. 또한 <b>classnames</b> 모듈을
+				이용하여 조건부 스타일링을 줄 것입니다. 체크가 되어 있다면 MdCheckBox
+				아이콘을 사용하여 우리가 만든 scss를 이용한 줄 그어진 모습을 보여줄
+				것이고, 체크가 되어있지 않다면 MdCheckBoxOutline 아이콘을 통해 검은색
+				체크박스를 보여줄 것입니다. 아이콘과 더불어 todos의 객체로 전달 받은
+				'text'를 같이 보여줍니다. 뒤의 클래스인 remove 클래스는 후에 구성할
+				것입니다.
+			</p>
 		</div>
 	);
 };
